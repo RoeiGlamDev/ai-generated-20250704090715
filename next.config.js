e8d7eec01ee10a7789614267b528b90798f710f3
RoeiGlamDev <Roeiglam2@gmail.com>
2025-07-04 16:07:17 +0700

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+module.exports = {
+  // Sentry configuration
+  sentry: {
+    // Use the @sentry/nextjs package to configure Sentry
+    configFile: './sentry.client.config.js',
+  },
+};
